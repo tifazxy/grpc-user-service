d03b374ecef983df20472584eed130af4072d23e
tifazxy <tifazxy@gmail.com>
2025-10-16 00:46:13 -0400

    add logging feature

--- services/gateway/main.py
+++ services/gateway/main.py
@@ -12,14 +12,38 @@ from services.gateway.auth import(
 from datetime import timedelta
 from services.gateway import utils
 from fastapi.security import OAuth2PasswordRequestForm
+from pathlib import Path
+import logging.config
+import json
 
+def initialize_logging(config_filename: str = 'logging_config.json'):
+    """load json logging configuration"""
+    current_file_path = Path(__file__).resolve() # using absolute path to find the file
+    current_dir = current_file_path.parent
+    config_path = current_dir.parent / config_filename
+
+    if Path(config_path).exists():
+        with open(config_path, 'rt') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config=config)
+
+        global logger
+        logger = logging.getLogger("app_logger")
+        logger.info("Logging initialize succeed!")
+    else:
+        # no config file the logging should work at least
+        logging.basicConfig(level=logging.WARNING)
+        print(f"warning:cannot find the logging configuration file in path {config_path}")
+
+
+initialize_logging()
 app = FastAPI()
 
 
 @app.post("/login", response_model=Token)
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user = authenticate_user(form_data.username, form_data.password) # TODO this username is actually email
-    print(user)
+    logger.info(user)
     if not user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect username or password")
     access_token = create_access_token(
@@ -31,7 +55,7 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
 
 @app.get("/me", response_model=dict)
 async def read_users_me(current_user: Annotated[dict, Depends(get_current_user)]):
-    print("read_users_me:",current_user)
+    logger.info("read_users_me:",current_user)
     return current_user
 
 @app.post("/users/add")
